50fc503d18c0552c9e8a9bf7a2ad9d449fe28a5e
sikanhe <sikanh@gmail.com>
2016-07-27 19:49:15 -0400

    updated readme

--- mix.exs
+++ mix.exs
@@ -7,7 +7,22 @@ defmodule Stripe.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package(),
+     description: description()]
+  end
+
+  def description do
+    "Stripe API Client for Elixir"
+  end
+
+  def package do
+    [
+      name: "stripe_elixir",
+      maintainers: ["Sikan He"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/sikanhe/stripe-elixir"}
+    ]
   end
 
   def application do
@@ -16,6 +31,11 @@ defmodule Stripe.Mixfile do
 
   defp deps do
     [{:httpoison, "~> 0.9.0"},
-     {:poison, "~> 2.2"}]
+     {:poison, "~> 2.2"},
+
+     # Docs
+     {:ex_doc, "~> 0.10", only: :dev},
+     {:earmark, "~> 0.1", only: :dev},
+     {:inch_ex, ">= 0.0.0", only: :dev}]
   end
 end
