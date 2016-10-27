7a04d14e43503197671819a9d8eaf814c44834a3
sikanhe <sikanh@gmail.com>
2016-10-26 23:22:41 -0400

    0.3.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Stripe.Mixfile do
 
   def project do
     [app: :stripe,
-     version: "0.3.3",
+     version: "0.3.4",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
