9a1ddebfb6217f9b6ffb7c9f2cdf1ca7bed9e572
Sikan He <sikanh@gmail.com>
2017-05-30 17:18:48 -0400

    abstract external account url function

--- lib/stripe/connect/account.ex
+++ lib/stripe/connect/account.ex
@@ -7,28 +7,32 @@ defmodule Stripe.Account do
     Stripe.request(:post, "#{endpoint()}/#{account_id}/reject", reason: reason)
   end
 
+  def external_account_endpoint(account_id, external_account_id \\ "") do 
+    "#{endpoint()}/#{account_id}/external_accounts/#{external_account_id}"
+  end 
+
   def create_externa_account(account_id, external_account: token_id) do
-    url = "#{endpoint()}/#{account_id}/external_accounts"
+    url = external_account_endpoint(account_id)
     Stripe.request(:post, url, external_account: token_id)
   end
 
   def retrieve_external_account(account_id, external_account_id) do
-    url = "#{endpoint()}/#{account_id}/external_accounts/#{external_account_id}"
+    url = external_account_endpoint(account_id, external_account_id)
     Stripe.request(:get, url)
   end
 
   def update_external_account(account_id, external_account_id, updates) do
-    url = "#{endpoint()}/#{account_id}/external_accounts/#{external_account_id}"
+    url = external_account_endpoint(account_id, external_account_id)
     Stripe.request(:post, url, updates)
   end
 
   def delete_external_account(account_id, external_account_id) do
-    url = "#{endpoint()}/#{account_id}/external_accounts/#{external_account_id}"
+    url = external_account_endpoint(account_id, external_account_id)
     Stripe.request(:delete, url)
   end
 
   def list_external_account(account_id, pagination_opts \\ []) do
-    url = "#{endpoint()}/#{account_id}/external_accounts"
+    url = external_account_endpoint(account_id)
     Stripe.request(:get, url, pagination_opts)
   end
 end
