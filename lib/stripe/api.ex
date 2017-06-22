9a4f1a6bdd46aa09f48be83e2844fc592d298cd6
Sikan He <sikanh@gmail.com>
2017-06-22 16:13:38 -0400

    incorrect optional_callback arg numbers

--- lib/stripe/api.ex
+++ lib/stripe/api.ex
@@ -4,5 +4,5 @@ defmodule Stripe.API do
   @callback create(Enum.t) :: Stripe.Request.t
   @callback update(id :: binary, updates :: Enum.t) :: Stripe.Request.t 
   @callback delete(id :: binary, options :: Enum.t) :: Stripe.Request.t
-  @optional_callbacks list: 0, retrieve: 0, create: 0, update: 0, delete: 0
+  @optional_callbacks list: 2, retrieve: 1, create: 1, update: 2, delete: 2
 end
