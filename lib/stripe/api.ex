cc9b939f4be1a0c8f611b30eece275bc41474678
Sikan He <sikanh@gmail.com>
2017-06-22 16:18:18 -0400

    list callback impl should only have 1 arg

--- lib/stripe/api.ex
+++ lib/stripe/api.ex
@@ -4,5 +4,5 @@ defmodule Stripe.API do
   @callback create(Enum.t) :: Stripe.Request.t
   @callback update(id :: binary, updates :: Enum.t) :: Stripe.Request.t 
   @callback delete(id :: binary, options :: Enum.t) :: Stripe.Request.t
-  @optional_callbacks list: 2, retrieve: 1, create: 1, update: 2, delete: 2
+  @optional_callbacks list: 1, retrieve: 1, create: 1, update: 2, delete: 2
 end
